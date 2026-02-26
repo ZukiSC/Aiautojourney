12d41e37a6f0c82b7270b63a51f46f921ff0cbda
Jieson DelaFuente <jiesonleobendelafuente@gmail.com>
2026-02-26 11:19:19 +0800

    Update digit.py

--- digit.py
+++ digit.py
@@ -1,18 +1,10 @@
-# Extract digits from the age input
-age = input("How old are you? ")
-digits = []
-for char in age:
-    if char.isdigit():
-        digits.append(char)
-
-print("You are " + ''.join(digits) + " years old.")
-
 #extract the capital letters from the name input
 Fname = input("What's your full name? ")
 capital_letters = []
 for char in Fname:
     if char.isupper():
         capital_letters.append(char)
+
 
 print("Your capital letters are: " + ''.join(capital_letters))
 
@@ -29,5 +21,15 @@ for char in Fname:
     if not char.isalnum() and not char.isspace():
         special_characers.append(char)
 print("Your special characters are: " + ''.join(special_characers))
+
+# Extract digits from the age input
+age = input("How old are you? ")
+digits = []
+for char in age:
+    if char.isdigit():
+        digits.append(char)
+
+print("You are " + ''.join(digits) + " years old.")
+
 
     
